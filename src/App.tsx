1f75727914f3a219603ab6eeb0579da4eecb0161
Jin Baek Hong <jinbaek@koreaspacedata.com>
2022-02-01 21:15:51 +0900

    init: twin macro

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const OnWorking = styled.div`
+  ${tw`w-full h-screen flex flex-col items-center justify-center bg-blue-400 text-white`}
+
+  .title {
+    ${tw`text-5xl font-bold mb-4`}
+  }
+
+  .message {
+    ${tw`text-sm font-light`}
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-      <div className="w-full h-screen flex flex-col items-center justify-center bg-blue-400 text-white">
-        <h1 className="text-5xl font-bold mb-4">VFLO</h1>
-        <p className="text-sm font-light">어떤 모습일지 기대하세요.</p>
-      </div>
+      <OnWorking>
+        <h1 className="title">VFLO</h1>
+        <p className="message">어떤 모습일지 기대하세요.</p>
+      </OnWorking>
     </div>
   );
 }
