11dd20e68e30e0780ed6ef4854525c15828f2927
Jin Baek Hong <jinbaek@koreaspacedata.com>
2022-02-01 20:32:28 +0900

    etc: prettier tab 말고 space 2칸 쓰기로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 function App() {
-	return (
-		<div className="App">
-			<div className="w-full h-screen flex flex-col items-center justify-center bg-blue-400 text-white">
-				<h1 className="text-5xl font-bold mb-4">VFLO</h1>
-				<p className="text-sm font-light">어떤 모습일지 기대하세요.</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div className="w-full h-screen flex flex-col items-center justify-center bg-blue-400 text-white">
+        <h1 className="text-5xl font-bold mb-4">VFLO</h1>
+        <p className="text-sm font-light">어떤 모습일지 기대하세요.</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
