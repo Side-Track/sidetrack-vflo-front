8a0de4e7ddeefdd74ef9e5e1ce030962eaaff79d
Jin Baek Hong <jinbaek@koreaspacedata.com>
2022-02-06 19:18:27 +0900

    ui: user example page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -5,7 +5,7 @@ const AuthPage = () => {
     <div>
       <h1>Auth Page</h1>
       <button>
-        <Link to="/signup">회원가입</Link>
+        <Link to="/auth/signup">회원가입</Link>
       </button>
       <button>
         <Link to="/editor">에디터 페이지 체험하기</Link>
